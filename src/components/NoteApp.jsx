2d42152f9dda78224d77954c4e5bb94af1e433db
Marsel Adenito R <72617344+marseladeraharjo@users.noreply.github.com>
2023-11-10 22:06:31 +0700

    add delete feature

--- src/components/NoteApp.jsx
+++ src/components/NoteApp.jsx
@@ -11,26 +11,33 @@ class NoteApp extends React.Component {
     this.state = {
       notes: getData(),
     };
+
+    this.onDeleteHandler = this.onDeleteHandler.bind(this);
+  }
+
+  onDeleteHandler(id) {
+    const notes = this.state.notes.filter((note) => note.id !== id);
+    this.setState({ notes });
   }
 
   render() {
     return (
       <div className="note-app">
         <div className="note-app__header">
-          <h1>EmNotes</h1>
+          <h1>notes.co</h1>
           <NoteSearch />
         </div>
         <div className="note-app__body">
           <NoteInput />
           <h2>Active Notes</h2>
-          <NoteList notes={this.state.notes} />
+          <NoteList notes={this.state.notes} onDelete={this.onDeleteHandler} />
           <h2>Archive Notes</h2>
           <NoteList notes={this.state.notes} />
         </div>
         <div className="note-app__footer">
           <p>
             Made by{" "}
-            <a href="https://github.com/marseladeraharjo" target="_blank">
+            <a href="https://github.com/marseladeraharjo" target="_blank" rel="noreferrer">
               @marseladeraharjo
             </a>
           </p>
--- src/components/NoteSearch.jsx
+++ src/components/NoteSearch.jsx
@@ -7,7 +7,7 @@ class NoteSearch extends React.Component {
   render() {
     return (
       <div className="note-search">
-        <input type="text" placeholder="Cari catatan ..." />
+        <input type="text" placeholder="Search note ..." />
       </div>
     );
   }
