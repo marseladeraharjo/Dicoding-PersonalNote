109cce3424abd52afe0f8538958a1dbad9038cd6
Marsel Adenito R <72617344+marseladeraharjo@users.noreply.github.com>
2023-11-24 20:36:29 +0700

    add empty message

--- src/components/NoteItemContent.jsx
+++ src/components/NoteItemContent.jsx
@@ -16,7 +16,7 @@ function NoteItemContent({ title, createdAt, body }) {
     <div className="note-item__content">
       <h3 className="note-item__title">{title}</h3>
       <p className="note-item__date">{createdAt}</p>
-      <p className="note-item__note">{body}</p>
+      <p className="note-item__body">{body}</p>
     </div>
   );
 }
--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -3,11 +3,14 @@ import NoteItem from "./NoteItem";
 
 function NoteList({ notes, onDelete }) {
   return (
-    <div className="note-list">
-      {notes.map((note) => (
-        <NoteItem key={note.id} id={note.id} onDelete={onDelete} {...note} />
-      ))}
-    </div>
+    <>
+      <div className="note-list">
+        {notes.map((note) => (
+          <NoteItem key={note.id} id={note.id} onDelete={onDelete} {...note} />
+        ))}
+      </div>
+      <p className="note-list__empty-message">No notes here</p>
+    </>
   );
 }
 
