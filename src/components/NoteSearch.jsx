8db2b611da74d1cbbd99a870062123b076680ef9
Marsel Adenito R <72617344+marseladeraharjo@users.noreply.github.com>
2023-11-28 21:35:53 +0700

    remove unused comment

--- src/components/NoteSearch.jsx
+++ src/components/NoteSearch.jsx
@@ -1,35 +1,6 @@
 /* eslint-disable react/prop-types */
 import React from "react";
 
-// class NoteSearch extends React.Component {
-//   constructor(props) {
-//     super(props);
-
-//     this.state = {
-//       searchInput: "",
-//     };
-
-//     this.onSearchChangeEventHandler = this.onSearchChangeEventHandler.bind(this);
-//   }
-
-//   onSearchChangeEventHandler(event) {
-//     // this.props.searchNote(this.state);
-//     this.setState(() => {
-//       return {
-//         searchInput: event.target.value,
-//       };
-//     });
-//   }
-
-//   render() {
-//     return (
-//       <div className="note-search">
-//         <input type="text" placeholder="Search note ..." value={this.state.searchInput} onChange={this.onSearchChangeEventHandler} />
-//       </div>
-//     );
-//   }
-// }
-
 function NoteSearch({ onSearch }) {
   const onSearchChangeEventHandler = (event) => {
     onSearch(event.target.value);
